c459e63811fd94c5dc69eeae3e4c6b11645add22
Michael Perryment <81166066+Mike7P@users.noreply.github.com>
2021-08-20 13:30:34 +0700

    Add files via upload

--- SQL_CODE-Joins.sql
+++ SQL_CODE-Joins.sql
@@ -1,3 +1,5 @@
+INNER JOINS
+
 SELECT reg_id, Logins.name, log_id From Registrations
 INNER JOIN Logins
 ON Registrations.name = Logins.name;
@@ -12,3 +14,21 @@ FROM payment
 INNER JOIN customer
 ON customer.customer_id = payment.customer_id;
 #Use . notation when the colounm you select is in both tables.
+
+
+FULL OUTER JOINS
+
+SELECT * FROM regestrations
+FULL OUTER JOIN Logins
+ON regestrations.name = Logins.name;
+
+
+FULL OUTER JOINS BELOW
+(Gets rows unique to either table. Not in both tables)
+The exact opposite to an INNER JOIN
+
+SELECT * FROM regestrations
+FULL OUTER JOIN Logins
+ON regestrations.name = Logins.name
+WHERE regestrations.name is null
+OR Logins.name is null
